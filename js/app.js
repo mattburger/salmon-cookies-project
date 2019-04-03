b4953b775623d650c8d8100370f88f79982b8168
Matt Burger <matt.burger1@outlook.com>
2019-04-03 15:52:12 -0700

    added store totals back to table. new store append after hourly totals. needs fixed

--- js/app.js
+++ js/app.js
@@ -8,21 +8,26 @@ var capitol = ['Capitol',20, 38, 2.3];
 var alki = ['Alki',2,16,4.6];
 var dataSet = [pike,seaTac,seattle,capitol,alki];
 var hData = ['Location'];
-for(var i = 6; i < 12; i++)
+var makeHData = function()
 {
-  hData.push(i + 'am');
-}
-for(i = 12; i < 21; i++)
-{
-  if(i > 12)
+  for(var i = 6; i < 12; i++)
   {
-    hData.push((i-12) + 'pm');
+    hData.push(i + 'am');
   }
-  else
+  for(i = 12; i < 21; i++)
   {
-    hData.push(i + 'pm');
+    if(i > 12)
+    {
+      hData.push((i-12) + 'pm');
+    }
+    else
+    {
+      hData.push(i + 'pm');
+    }
   }
-}
+  hData.push('Total Cookies/Store');
+};
+makeHData();
 
 function SalmonCookieLocation(name,minCust,maxCust,avgCook)
 {
@@ -31,6 +36,7 @@ function SalmonCookieLocation(name,minCust,maxCust,avgCook)
   this.minCustomers = minCust;
   this.maxCustomer = maxCust;
   this.avgCookies = avgCook;
+  this.dayTotalCookies = 0;
   this.customersHour = [];
   this.cookiesHour = [];
   this.populateCustHr = function ()
@@ -48,6 +54,14 @@ function SalmonCookieLocation(name,minCust,maxCust,avgCook)
       this.cookiesHour.push(Math.ceil(this.customersHour[i] * this.avgCookies));
     }
   };
+  this.calcCookieDayTotal = function ()
+  {
+    for(var i = 0; i < this.cookiesHour.length;i++)
+    {
+      this.dayTotalCookies += this.cookiesHour[i];
+    }
+
+  };
 
   allLocations.push(this);
 }
@@ -61,7 +75,7 @@ function rand(min, max)
 
 var setLocData = function ()
 {
-  for(i = 0; i < dataSet.length; i++)
+  for(var i = 0; i < dataSet.length; i++)
   {
     new SalmonCookieLocation(dataSet[i][0],dataSet[i][1],dataSet[i][2],dataSet[i][3]);
   }
@@ -69,13 +83,14 @@ var setLocData = function ()
   {
     allLocations[i].populateCustHr();
     allLocations[i].populateCookiesAnHr();
+    allLocations[i].calcCookieDayTotal();
   }
 };
 setLocData();
 
 function totalCookiesPerHourAllLoc()
 {
-  var tmpHourlyCookieTotal = ['Total Cookies'];
+  var tmpHourlyCookieTotal = ['Total Cookies/hr'];
   for(var i = 0; i < allLocations[0].cookiesHour.length;i++)
   {
     tmpHourlyCookieTotal.push(0);
@@ -101,6 +116,8 @@ SalmonCookieLocation.prototype.render = function()
     tdEl.textContent = 'Customers: ' + this.customersHour[i] + '\nCookies: ' + this.cookiesHour[i];
     trEl.appendChild(tdEl); 
   }
+  tdEl = document.createElement('td');
+  tdEl.textContent = this.dayTotalCookies;
   trEl.appendChild(tdEl);
   daTable.appendChild(trEl);
 };
@@ -149,9 +166,9 @@ function handleSubmit(event)
 {
   event.preventDefault();
   var tmpStoreName = event.target.sName.value;
-  var tmpCustomerMinData = event.target.custMinD.value;
-  var tmpCustomerMaxData = event.target.custMaxD.value;
-  var tmpCookieData = event.target.cookD.value;
+  var tmpCustomerMinData = parseInt(event.target.custMinD.value);
+  var tmpCustomerMaxData = parseInt(event.target.custMaxD.value);
+  var tmpCookieData = parseFloat(event.target.cookD.value);
   if(!tmpStoreName || !tmpCustomerMinData || !tmpCustomerMaxData || !tmpCookieData)
   {
     alert('The fields must be filled to create a new store');
@@ -160,14 +177,15 @@ function handleSubmit(event)
 
   new SalmonCookieLocation(tmpStore.storeName,tmpStore.customerMinData,tmpStore.customerMaxData,tmpStore.cookieData);
 
-  allLocations[allLocations.length-1].populateCustHr;
-  allLocations[allLocations.length-1].populateCookiesAnHr;
+  allLocations[allLocations.length-1].populateCustHr();
+  allLocations[allLocations.length-1].populateCookiesAnHr();
+  console.log(allLocations[allLocations.length-4]);
+  console.log(allLocations[allLocations.length-1]);
 
   allLocations[allLocations.length-1].render();
 }
+//console.log(allLocations[allLocations.length-1]);
 /*form handling area*/
-
-
 
 makeHeaderRow(hData);
 renderLocations();
